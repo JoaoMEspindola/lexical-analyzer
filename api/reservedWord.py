eebf241457d07d54a8a0988dea1fdd86833afdd0
Joao <jpmaresp@gmail.com>
2024-07-04 20:26:30 -0300

    initial config

---
+++ api/reservedWord.py
@@ -0,0 +1,11 @@
+class ReservedWord:
+
+    def __init__(self, words) -> None:
+        self.words = words
+
+    def findReservedWord(self, word):
+        if word in self.words:
+            return True
+        return False
+    
+    
