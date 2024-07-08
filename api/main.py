febf94777bd64e22b3f48ee4424975dcb1423e98
Lucas Farinelli <lucasfcpinho@gmail.com>
2024-07-08 15:17:03 -0300

    Adding regex for string consts and comments

--- api/main.py
+++ api/main.py
@@ -5,6 +5,8 @@ patterns = regexPatterns.RegexPatterns()
 code = dataReader.readFullFile("./assets/test.txt")
 
 
-print("Palavras Reservadas:", patterns.match_reserved_words(code))
-print("Operadores:", patterns.match_operators(code))
-print("Símbolos Especiais:", patterns.match_special_symbols(code))
+print("Palavras Reservadas: ", patterns.match_reserved_words(code))
+print("Operadores: ", patterns.match_operators(code))
+print("Símbolos Especiais: ", patterns.match_special_symbols(code))
+print("Comentários: ", patterns.match_comments(code))
+print("Constantes de texto: ", patterns.match_text_consts(code))
--- api/regexPatterns.py
+++ api/regexPatterns.py
@@ -4,6 +4,8 @@ class RegexPatterns:
         self.reservedWords = re.compile(r'int|float|char|bool|void|if|else|for|while|scanf|printf|main|return\b')
         self.operators = re.compile(r'=|\+|-|\*|\/|%|&&|\|\||!|>=|<=|>|<|!=|==\b')
         self.specialSymbols = re.compile(r'\(|\)|\{|\}|\[|\]|;|,|\.\b')
+        self.comments = re.compile(r'//.*\n')
+        self.textConsts = re.compile(r'".*"')
 
     def match_reserved_words(self, text):
         return self.reservedWords.findall(text)
@@ -13,3 +15,9 @@ class RegexPatterns:
     
     def match_special_symbols(self, text):
         return self.specialSymbols.findall(text)
+    
+    def match_comments(self, text):
+        return self.comments.findall(text)
+    
+    def match_text_consts(self, text):
+        return self.textConsts.findall(text)
