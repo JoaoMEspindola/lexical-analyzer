929bdc502e371216a1abf2f6836223313ac79fa7
Joao <jpmaresp@gmail.com>
2024-07-08 18:29:09 -0300

    Added list_tokens and symbol_table and its informations are appending correctly

--- api/main.py
+++ api/main.py
@@ -3,10 +3,28 @@ import regexPatterns
 
 patterns = regexPatterns.RegexPatterns()
 code = dataReader.readFullFile("./assets/test.txt")
+token = ''
+list_tokens = []
+symbol_table = []
+id = 0
 
+for c in code:
+    if patterns.match_alpha_nums(c):
+        token += c
+    else:
+        if token:
+            if patterns.match_reserved_words(token):
+                list_tokens.append(token)    
+            else:
+                id += 1
+                list_tokens.append({'id': id})
+                symbol_table.append({id: token})
+            token = ''
+        if patterns.match_operators(c) or patterns.match_special_symbols(c):
+            list_tokens.append(c)
 
-print("Palavras Reservadas: ", patterns.match_reserved_words(code))
-print("Operadores: ", patterns.match_operators(code))
-print("Símbolos Especiais: ", patterns.match_special_symbols(code))
-print("Comentários: ", patterns.match_comments(code))
-print("Constantes de texto: ", patterns.match_text_consts(code))
+if token:
+    list_tokens.append(token)
+
+print(f'Lista de tokens: {list_tokens}')
+print(f'Tabela de símbolos: {symbol_table}')
--- api/regexPatterns.py
+++ api/regexPatterns.py
@@ -1,23 +1,27 @@
 import re
 class RegexPatterns:
     def __init__(self) -> None:
-        self.reservedWords = re.compile(r'int|float|char|bool|void|if|else|for|while|scanf|printf|main|return\b')
+        self.reservedWords = re.compile(r'int|float|char|bool|void|if|else|for|while|scanf|printf|main|return|string\b')
         self.operators = re.compile(r'=|\+|-|\*|\/|%|&&|\|\||!|>=|<=|>|<|!=|==\b')
-        self.specialSymbols = re.compile(r'\(|\)|\{|\}|\[|\]|;|,|\.\b')
+        self.specialSymbols = re.compile(r'"|\(|\)|\{|\}|\[|\]|;|,|\.\b')
         self.comments = re.compile(r'//.*\n')
-        self.textConsts = re.compile(r'".*"')
+        self.textConsts = re.compile(r'^[a-zA-Z_][a-zA-Z0-9_]*$')
+        self.alphanums = re.compile(r'^[a-zA-Z0-9]$')
 
     def match_reserved_words(self, text):
-        return self.reservedWords.findall(text)
+        return self.reservedWords.fullmatch(text)
     
     def match_operators(self, text):
-        return self.operators.findall(text)
+        return self.operators.fullmatch(text)
     
     def match_special_symbols(self, text):
-        return self.specialSymbols.findall(text)
+        return self.specialSymbols.fullmatch(text)
     
     def match_comments(self, text):
-        return self.comments.findall(text)
+        return self.comments.fullmatch(text)
     
     def match_text_consts(self, text):
-        return self.textConsts.findall(text)
+        return self.textConsts.fullmatch(text)
+    
+    def match_alpha_nums(self, text):
+        return self.alphanums.fullmatch(text)
