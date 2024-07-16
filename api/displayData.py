9943a5b75d78818690cc601aac9a60555ddec622
Lucas Farinelli <lucasfcpinho@gmail.com>
2024-07-16 00:31:58 -0300

    treating comments, new result display, no replicas in symbols table

---
+++ api/displayData.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import tkinter as tk
+from tkinter import ttk
+
+def displayResult(lista_tokens, tabela_simbolos):
+    tabela_tokens = pd.DataFrame({'Token': lista_tokens})
+    tabela_tokens.insert(0, 'Número', range(1, len(tabela_tokens) + 1))
+
+    tabela_simbolos_formatada = pd.DataFrame(list(tabela_simbolos.items()), columns=['Símbolo', 'Valor'])
+
+    root = tk.Tk()
+    root.title("Resultado")
+
+    frame_tokens = ttk.Frame(root)
+    frame_tokens.pack(padx=10, pady=10)
+
+    ttk.Label(frame_tokens, text="Lista de tokens").pack()
+
+    tree_tokens = ttk.Treeview(frame_tokens)
+    tree_tokens['columns'] = ('Número', 'Token')
+    tree_tokens.heading('#0', text='', anchor='w')
+    tree_tokens.heading('Número', text='Número')
+    tree_tokens.heading('Token', text='Token')
+
+    for index, row in tabela_tokens.iterrows():
+        tree_tokens.insert('', 'end', text='', values=(row['Número'], row['Token']))
+
+    tree_tokens.pack()
+
+    frame_simbolos = ttk.Frame(root)
+    frame_simbolos.pack(padx=10, pady=10)
+
+    ttk.Label(frame_simbolos, text="Tabela de Símbolos").pack()
+
+    tree_simbolos = ttk.Treeview(frame_simbolos)
+    tree_simbolos['columns'] = ('Símbolo', 'Valor')
+    tree_simbolos.heading('#0', text='', anchor='w')
+    tree_simbolos.heading('Símbolo', text='Símbolo')
+    tree_simbolos.heading('Valor', text='Valor')
+
+    for index, row in tabela_simbolos_formatada.iterrows():
+        tree_simbolos.insert('', 'end', text='', values=(row['Símbolo'], row['Valor']))
+
+    tree_simbolos.pack()
+    root.mainloop()
--- api/main.py
+++ api/main.py
@@ -1,30 +1,66 @@
 import dataReader
 import regexPatterns
+import displayData
+
+def treatComment(code, initialIndex):
+    commentLine = code[initialIndex:]
+
+    onlyCommentLine = ''
+    for i in commentLine:
+        if i != '\n':
+            onlyCommentLine = onlyCommentLine + i
+        else:
+            break 
+
+    finalIndex = initialIndex + len(onlyCommentLine)
+    return onlyCommentLine, finalIndex
 
 patterns = regexPatterns.RegexPatterns()
-code = dataReader.readFullFile("./assets/test.txt")
+code = dataReader.readFullFile("../assets/test.txt")
 token = ''
 list_tokens = []
-symbol_table = []
+symbol_table = {}
 id = 0
+last_char = ''
+index = 0
+foundComment = False
+diff = 0
 
 for c in code:
-    if patterns.match_alpha_nums(c):
-        token += c
+    if c == '/' and last_char == '/':
+        onlyCommentLine, finalIndex = treatComment(code, index + 1)
+        foundComment = True
+        diff = finalIndex - index
+        last_char = ''
+        list_tokens.append('/')
+        list_tokens.append(onlyCommentLine)
+    
+    last_char = c
+    
+    if diff == 0:
+        if patterns.match_alpha_nums(c):
+            token += c
+        else:
+            if token:
+                if patterns.match_reserved_words(token):
+                    list_tokens.append(token)    
+                else:
+                    id += 1
+                    if not token in symbol_table:
+                        symbol_table[token] = id
+                    else:
+                        id -= 1
+
+                    list_tokens.append(f"'id': {symbol_table[token]}")
+                token = ''
+            if patterns.match_operators(c) or patterns.match_special_symbols(c):
+                list_tokens.append(c)
     else:
-        if token:
-            if patterns.match_reserved_words(token):
-                list_tokens.append(token)    
-            else:
-                id += 1
-                list_tokens.append({'id': id})
-                symbol_table.append({id: token})
-            token = ''
-        if patterns.match_operators(c) or patterns.match_special_symbols(c):
-            list_tokens.append(c)
+        diff -= 1
+
+    index += 1
 
 if token:
     list_tokens.append(token)
 
-print(f'Lista de tokens: {list_tokens}')
-print(f'Tabela de símbolos: {symbol_table}')
+displayData.displayResult(list_tokens, symbol_table)
